f67f87d2047dd3f4d959d18705e76e95a725e0ee
Ejercicios One G8 <ma.toro.romo@gmail.com>
2025-01-20 17:15:10 -0300

    actualiza app.js
    actualiza encabezado con fecha y autor

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// MATR
+// 20-ENE-2025
 /*
 Esta aplicación permite a los usuarios ingresar nombres de amigos en una lista 
 para luego realizar un sorteo aleatorio y determinar quién es el "amigo secreto".
